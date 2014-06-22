fd3ba9b8ca4a428f28fa68cda353a24c5dc17193
Thomas Misch <thomas.misch@mailbox.tu-berlin.de>
2014-06-22 12:08:48 +0200

    added to Stock class

--- assignment4/src/main/java/server/Stock.java
+++ assignment4/src/main/java/server/Stock.java
@@ -2,12 +2,43 @@ package server;
 
 import java.sql.Timestamp;
 
+import java.io.Serializable;
 
+public class Stock implements Serializable {
 
-public class Stock {
-    
     private String name;
     private double price;
     private Timestamp timestamp;
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public double getPrice() {
+	return price;
+    }
+
+    public void setPrice(double price) {
+	this.price = price;
+    }
+
+    public Timestamp getTimestamp() {
+	return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+	this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+	return new StringBuffer("Stock: ").append(this.name)
+		.append(" " + this.price)
+		.append(" " + this.timestamp.toString()).toString();
+    }
 
 }
